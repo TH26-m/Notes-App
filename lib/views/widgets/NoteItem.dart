07b8a8f227e189141c7a82f48fbd6cca22e77b04
tasneem <tasneem@gmail.com>
2025-03-20 05:40:24 +0200

    added note_list_view with the items

---
+++ lib/views/widgets/NoteItem.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      padding:const EdgeInsets.all(24),
+      decoration:const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10),),
+        color: Color.fromARGB(255, 245, 181, 91)
+      ),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            title:const  Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text("Flutter tips" , style: TextStyle(color: Colors.black , fontSize: 20 , fontWeight: FontWeight.w600),),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text("Build your Career with Tharwat Samy" , style:  TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 16),),
+            ),
+            trailing: const Icon(Icons.delete , color: Colors.black, size: 30,),
+          ),
+          Text("May 21,2022" , style: TextStyle(color: Colors.black.withOpacity(0.5)),) ,
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/Notes-_view_body.dart
+++ lib/views/widgets/Notes-_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:notes_app/views/widgets/NoteItem.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
 class NoteViewBody extends StatelessWidget {
@@ -13,9 +15,24 @@ class NoteViewBody extends StatelessWidget {
         children: [
           //SizedBox( ),
           customAppBAr(),
+          Expanded(child: NoteItemsLIst(),) ,
         ],
       ),
     );
+  }
+}
+
+class NoteItemsLIst extends StatelessWidget {
+  const NoteItemsLIst({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context , i){
+      return const Padding(
+        padding:  EdgeInsets.only(bottom: 16),
+        child: NoteItem(),
+      );
+    }) ;
   }
 }
 
